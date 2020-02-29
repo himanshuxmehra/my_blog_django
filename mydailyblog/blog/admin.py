e949d182cdc4f4c115fe72f08314e167fff92e36
Himanshu Mehra <himanshu24mehra@gmail.com>
2020-02-29 17:22:11 +0530

    🚀 Blog created

--- mydailyblog/blog/admin.py
+++ mydailyblog/blog/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'created_on', 'updated_on', 'status')
+    list_filter = ('status',)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
